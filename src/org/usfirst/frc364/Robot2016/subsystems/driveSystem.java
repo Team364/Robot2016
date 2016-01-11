a1213d84264796ed1bcf87348117507626c3f405
Sam Boggs <samboggs11@yahoo.com>
2016-01-11 17:53:27 -0600

    Drive code fixed.

--- src/org/usfirst/frc364/Robot2016/subsystems/driveSystem.java
+++ src/org/usfirst/frc364/Robot2016/subsystems/driveSystem.java
@@ -35,7 +35,7 @@ public class driveSystem extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-    public void initDefaultCommand() {s
+    public void initDefaultCommand() {
     		
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
